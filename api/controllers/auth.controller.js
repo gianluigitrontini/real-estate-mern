cc8fbd5b82153aff84c02e4b16f596fc32c2e3fb
Gianluigi Trontini <gianluigitrontini@gmail.com>
2023-10-20 10:44:30 +0200

    Implementa gestione errori e crypt password.

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const user = new User({ username, email, password: hashedPassword });
+
+  try {
+    await user.save();
+    res.status(201).send({ message: "L'utente è stato creato con successo" });
+  } catch (error) {
+    next(errorHandler(error.statusCode, error.message));
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
@@ -22,3 +24,18 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+/**
+ * Error handling
+ */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
