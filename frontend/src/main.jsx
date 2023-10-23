6e2335e5a4ecdff50a759533800aea8f2524c8eb
Gianluigi Trontini <gianluigitrontini@gmail.com>
2023-10-23 14:17:12 +0200

    Implementazione redux-persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
